6f211ee47fcf0c5801539b98a459fff6426de238
MikeWolski <wolski17@gmail.com>
2024-11-14 16:04:44 -0500

    Added alert for aks node memory

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -18,3 +18,24 @@ resource "azurerm_monitor_metric_alert" "cpu_alert" {
     action_group_id = var.action_group_id
   }
 }
+
+resource "azurerm_monitor_metric_alert" "memory_alert" {
+  name                = "aks-memory-alert"
+  resource_group_name = var.resource_group_name
+  scopes              = [var.aks_id]
+  severity            = 2
+  frequency           = "PT5M"
+  window_size         = "PT5M"
+
+  criteria {
+    metric_namespace = "Microsoft.ContainerService/managedClusters"
+    metric_name      = "node_memory_rss_percentage"
+    aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 80
+  }
+
+  action {
+    action_group_id = var.action_group_id
+  }
+}
